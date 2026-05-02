78a7ab45873cc193dd18cd72c4732bd8ba1ae0ea
Shubhajit Chatterjee <chatterjee.shubhajit01@gmail.com>
2026-05-02 23:46:49 +0530

    Remove github pages

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -1,11 +1,10 @@
-import { reactRouter } from '@react-router/dev/vite';
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
-import mdx from 'fumadocs-mdx/vite';
-import * as MdxConfig from './source.config';
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import mdx from "fumadocs-mdx/vite";
+import * as MdxConfig from "./source.config";
 
 export default defineConfig({
-  base: "/react-sidefx/",
   plugins: [mdx(MdxConfig), tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
